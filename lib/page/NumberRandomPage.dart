80e91ce8731ad7eb488aa2f2914b0ea21f1abff2
notheartza <not.102@hotmail.com>
2021-03-24 13:35:13 +0700

    add

---
+++ lib/page/NumberRandomPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class NumberRandomPage extends StatefulWidget {
+  NumberRandomPage({Key key}) : super(key: key);
+
+  @override
+  _NumberRandomPageState createState() => _NumberRandomPageState();
+}
+
+class _NumberRandomPageState extends State<NumberRandomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );}
+  }
